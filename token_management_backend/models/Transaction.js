a8af781f2c3a10469da9b9208f60ae70ec02c948
bom254 <bevonmokuas@gmail.com>
2025-03-11 12:13:09 +0000

    added an index.js file to call transaction and user

--- token_management_backend/models/Transaction.js
+++ token_management_backend/models/Transaction.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const TransactionSchema = new mongoose.Schema({
     from: {type: String, required: true},
-    to: {type: String, require: true},
+    to: {type: String, required: true},
     amount: {type: Number, required: true},
     timestamp: {type: Date, default: Date.now},
 });
 
-module.exports = mongoose.model('Transaction', TransactionSchema);
+module.exports = mongoose.model("Transaction", TransactionSchema);
